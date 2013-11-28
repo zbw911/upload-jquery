666b6ebf931bdeea89765cb815bd09348b6b495f
zbw911 <zbw911@qq.com>
2013-11-29 00:21:49 +0800

    Update jquery.upload.js

--- jquery.upload.js
+++ jquery.upload.js
@@ -1,72 +1,75 @@
-﻿/**
+/**
  * jQuery upload v1.2
  * http://www.ponxu.com
  *
  * @author xwz
+ * @modify zbw911
+
  */
-(function($) {
-	var noop = function(){ return true; };
-	var frameCount = 0;
-	
-	$.uploadDefault = {
-		url: '',
-		fileName: 'filedata',
-		dataType: 'json',
-		params: {},
-		onSend: noop,
-		onComplate: noop
-	};
+(function ($) {
+    var noop = function () { return true; };
+    var frameCount = 0;
 
-	$.upload = function(options) {
-		var opts = $.extend(jQuery.uploadDefault, options);
-		if (opts.url == '') {
-			return;
-		}
-		
-		var canSend = opts.onSend();
-		if (!canSend) {
-			return;
-		}
-		
-		var frameName = 'upload_frame_' + (frameCount++);
-		var iframe = $('<iframe style="position:absolute;top:-9999px" />').attr('name', frameName);
-		var form = $('<form method="post" style="display:none;" enctype="multipart/form-data" />').attr('name', 'form_' + frameName);
-		form.attr("target", frameName).attr('action', opts.url);
-		
-		// form中增加数据域
-		var formHtml = '<input type="file" name="' + opts.fileName + '" onchange="onChooseFile(this)">';
-		for (key in opts.params) {
-			formHtml += '<input type="hidden" name="' + key + '" value="' + opts.params[key] + '">';
-		}
-		form.append(formHtml);
+    $.uploadDefault = {
+        url: '',
+        fileName: 'filedata',
+        dataType: 'json',
+        params: {},
+        onSend: noop,
+        onComplate: noop
+    };
 
-		iframe.appendTo("body");
-		form.appendTo("body");
-		
-		// iframe 在提交完成之后
-		iframe.load(function() {
-		 iframe.unbind().load(function () {
-                var contents = $(this).contents().get(0);
-                var data = $(contents).find('body').text();
-                if ('json' == opts.dataType) {
-                    data = window.eval('(' + data + ')');
-                }
-                opts.onComplate(data);
-                setTimeout(function () {
+    $.upload = function (options) {
+        var opts = $.extend(jQuery.uploadDefault, options);
+        if (opts.url == '') {
+            return;
+        }
+
+        var canSend = opts.onSend();
+        if (!canSend) {
+            return;
+        }
+
+        var frameName = 'upload_frame_' + (frameCount++);
+        var iframe = $('<iframe style="position:absolute;top:-9999px" />').attr('name', frameName);
+        var form = $('<form method="post" style="display:none;" enctype="multipart/form-data" />').attr('name', 'form_' + frameName);
+        form.attr("target", frameName).attr('action', opts.url);
+
+        // form中增加数据域
+        var formHtml = '<input type="file" name="' + opts.fileName + '" onchange="onChooseFile(this)">';
+        for (key in opts.params) {
+            formHtml += '<input type="hidden" name="' + key + '" value="' + opts.params[key] + '">';
+        }
+        form.append(formHtml);
+
+        iframe.appendTo("body");
+        form.appendTo("body");
+
+        // iframe 在提交完成之后
+        iframe.load(function () {
+
+            var frameWin = iframe[0].contentWindow ||
+                            (iframe[0].contentDocument && iframe[0].contentDocument.window);
+
+            iframe
+                .unbind()
+                .load(function () {
+                    var resp = frameWin.name;
                     iframe.remove();
-                    form.remove();
-                }, 5000);
-            });
-		});
-		
-		// 文件框
-		var fileInput = $('input[type=file][name=' + opts.fileName + ']', form);
-		fileInput.click();
-	};
+                    opts.onComplate(resp);
+                });
+
+            frameWin.location = 'about:blank';
+        });
+
+        // 文件框
+        var fileInput = $('input[type=file][name=' + opts.fileName + ']', form);
+        fileInput.click();
+    };
 })(jQuery);
 
 // 选中文件, 提交表单(开始上传)
-var onChooseFile = function(fileInputDOM) {
-	var form = $(fileInputDOM).parent();
-	form.submit();
+var onChooseFile = function (fileInputDOM) {
+    var form = $(fileInputDOM).parent();
+    form.submit();
 };
